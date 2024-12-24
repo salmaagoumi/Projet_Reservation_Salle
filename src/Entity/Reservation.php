03b4343d31c212c90d2268cca0e8333ba29db278
GeniusCode-lang <amine.web.analyste@gmail.com>
2024-12-24 22:45:19 +0100

    Classe Reservation

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -1,178 +1,129 @@
 <?php
 
-namespace App\Controller;
+namespace App\Entity;
 
-use App\Entity\Reservation;
-use App\Form\ReservationType;
 use App\Repository\ReservationRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
 
-use App\Entity\Promos;
-use App\Entity\Salle;
-use App\Entity\Matiere;
-use App\Entity\Enseignant;
-use Doctrine\ORM\Query\Parameter;
-
-
-
-#[Route('/reservation')]
-final class ReservationController extends AbstractController
+#[ORM\Entity(repositoryClass: ReservationRepository::class)]
+class Reservation
 {
-    #[Route(name: 'app_reservation_index', methods: ['GET'])]
-    public function index(ReservationRepository $reservationRepository): Response
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Promos $promos = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    private ?Enseignant $enseignant = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Matiere $matiere = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Salle $salle = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $jour = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $heur_deb = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $heur_fin = null;
+
+    public function getId(): ?int
     {
-        return $this->render('reservation/index.html.twig', [
-            'reservations' => $reservationRepository->findAll(),
-        ]);
+        return $this->id;
     }
 
-    #[Route('/new', name: 'app_reservation_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function getPromos(): ?Promos
     {
-        $reservation = new Reservation();
-        $form = $this->createForm(ReservationType::class, $reservation);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-        // copier l code
-        $promotions = $entityManager->getRepository(Promos::class)->find($reservation->getPromos()->getId());
-        $salle = $entityManager->getRepository(Salle::class)->find($reservation->getSalle()->getId());
-        $enseignant = $entityManager->getRepository(Enseignant::class)->find($reservation->getEnseignant()->getId());
-        $jour = $reservation->getJour();
-        $heur_deb = $reservation->getHeurDeb();
-        $heur_fin = $reservation->getHeurFin();
-
-// Check if the room is free for the teacher, promos, and salle on the same date and time range
-        $parameters = new ArrayCollection([
-            new Parameter('salle', $salle),
-            new Parameter('promos', $promotions),
-            new Parameter('enseignant', $enseignant),
-            new Parameter('jour', $jour),
-            new Parameter('heurDeb', $heur_deb),
-            new Parameter('heurFin', $heur_fin),
-        ]);
-
-        $conflictingReservation = $entityManager->getRepository(Reservation::class)->createQueryBuilder('r')
-            ->where('r.salle = :salle')
-            ->orWhere('r.promos = :promos')
-            ->orWhere('r.enseignant = :enseignant')
-            ->andWhere('r.jour = :jour')
-            ->andWhere('r.heur_deb < :heurFin AND r.heur_fin > :heurDeb') // Check for overlapping times
-            ->setParameters($parameters)
-            ->getQuery()
-            ->getOneOrNullResult();
-
-
-        if ($conflictingReservation) {
-            return new Response('Cette salle est déjà réservée pour ce créneau!', 400);
-        }
-
-        // If no conflicts, proceed with saving the reservation
-        //$reservation->setSalle($salle);
-        //$reservation->setPromos($promotions);
-        //$reservation->setEnseignant($enseignant);
-
-        $entityManager->persist($reservation);
-        $entityManager->flush();
-
-        //return new Response('Réservation effectuée avec succès!');
-
-        return $this->redirectToRoute('app_reservation_index', [], Response::HTTP_SEE_OTHER);
+        return $this->promos;
     }
 
-        return $this->render('reservation/new.html.twig', [
-            'reservation' => $reservation,
-            'form' => $form,
-        ]);
+    public function setPromos(?Promos $promos): static
+    {
+        $this->promos = $promos;
+
+        return $this;
     }
 
-    #[Route('/{id}', name: 'app_reservation_show', methods: ['GET'])]
-    public function show(Reservation $reservation): Response
+    public function getEnseignant(): ?Enseignant
     {
-        return $this->render('reservation/show.html.twig', [
-            'reservation' => $reservation,
-        ]);
+        return $this->enseignant;
     }
 
-    #[Route('/{id}/edit', name: 'app_reservation_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Reservation $reservation, EntityManagerInterface $entityManager): Response
+    public function setEnseignant(?Enseignant $enseignant): static
     {
-        $form = $this->createForm(ReservationType::class, $reservation);
-        $form->handleRequest($request);
+        $this->enseignant = $enseignant;
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $promotions = $entityManager->getRepository(Promos::class)->find($reservation->getPromos()->getId());
-            $salle = $entityManager->getRepository(Salle::class)->find($reservation->getSalle()->getId());
-            $enseignant = $entityManager->getRepository(Enseignant::class)->find($reservation->getEnseignant()->getId());
-            $jour = $reservation->getJour();
-            $heur_deb = $reservation->getHeurDeb();
-            $heur_fin = $reservation->getHeurFin();
-            // copier l code
-            $promotions = $entityManager->getRepository(Promos::class)->find($reservation->getPromos()->getId());
-            $salle = $entityManager->getRepository(Salle::class)->find($reservation->getSalle()->getId());
-            $enseignant = $entityManager->getRepository(Enseignant::class)->find($reservation->getEnseignant()->getId());
-            $jour = $reservation->getJour();
-            $heur_deb = $reservation->getHeurDeb();
-            $heur_fin = $reservation->getHeurFin();
-
-// Check if the room is free for the teacher, promos, and salle on the same date and time range
-            $parameters = new ArrayCollection([
-                new Parameter('salle', $salle),
-                new Parameter('promos', $promotions),
-                new Parameter('enseignant', $enseignant),
-                new Parameter('jour', $jour),
-                new Parameter('heurDeb', $heur_deb),
-                new Parameter('heurFin', $heur_fin),
-            ]);
-
-            $conflictingReservation = $entityManager->getRepository(Reservation::class)->createQueryBuilder('r')
-                ->where('r.salle = :salle')
-                ->orWhere('r.promos = :promos')
-                ->orWhere('r.enseignant = :enseignant')
-                ->andWhere('r.jour = :jour')
-                ->andWhere('r.heurDeb < :heurFin AND r.heurFin > :heurDeb') // Check for overlapping times
-                ->setParameters($parameters)
-                ->getQuery()
-                ->getOneOrNullResult();
-
-
-
-            if ($conflictingReservation) {
-                return new Response('Cette salle est déjà réservée pour ce créneau!', 400);
-            }
-
-            // If no conflicts, proceed with saving the reservation
-            //$reservation->setSalle($salle);
-            //$reservation->setPromos($promotions);
-            //$reservation->setEnseignant($enseignant);
-
-            $entityManager->persist($reservation);
-            $entityManager->flush();
-
-            //return new Response('Réservation effectuée avec succès!');
-
-            return $this->redirectToRoute('app_reservation_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('reservation/edit.html.twig', [
-            'reservation' => $reservation,
-            'form' => $form,
-        ]);
+        return $this;
     }
 
-    #[Route('/{id}', name: 'app_reservation_delete', methods: ['POST'])]
-    public function delete(Request $request, Reservation $reservation, EntityManagerInterface $entityManager): Response
+    public function getMatiere(): ?Matiere
     {
-        if ($this->isCsrfTokenValid('delete'.$reservation->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($reservation);
-            $entityManager->flush();
-        }
+        return $this->matiere;
+    }
 
-        return $this->redirectToRoute('app_reservation_index', [], Response::HTTP_SEE_OTHER);
+    public function setMatiere(?Matiere $matiere): static
+    {
+        $this->matiere = $matiere;
+
+        return $this;
+    }
+
+    public function getSalle(): ?Salle
+    {
+        return $this->salle;
+    }
+
+    public function setSalle(?Salle $salle): static
+    {
+        $this->salle = $salle;
+
+        return $this;
+    }
+
+    public function getJour(): ?\DateTimeInterface
+    {
+        return $this->jour;
+    }
+
+    public function setJour(\DateTimeInterface $jour): static
+    {
+        $this->jour = $jour;
+
+        return $this;
+    }
+
+    public function getHeurDeb(): ?\DateTimeInterface
+    {
+        return $this->heur_deb;
+    }
+
+    public function setHeurDeb(\DateTimeInterface $heur_deb): static
+    {
+        $this->heur_deb = $heur_deb;
+
+        return $this;
+    }
+
+    public function getHeurFin(): ?\DateTimeInterface
+    {
+        return $this->heur_fin;
+    }
+
+    public function setHeurFin(\DateTimeInterface $heur_fin): static
+    {
+        $this->heur_fin = $heur_fin;
+
+        return $this;
     }
 }
