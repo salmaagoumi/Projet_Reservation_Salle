1d6e94f3a06891a085e7e8595951320e10cca5d6
Wissal Wardighi <w.wardighi@esisa.ac.ma>
2024-12-25 11:05:06 +0100

    Ajout des classes Enseignant et Enseigne dans src/Entity

--- src/Entity/Enseignant.php
+++ src/Entity/Enseignant.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\EnseignantRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EnseignantRepository::class)]
@@ -13,8 +15,116 @@ class Enseignant
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    /**
+     * @var Collection<int, Enseigne>
+     */
+    #[ORM\OneToMany(targetEntity: Enseigne::class, mappedBy: 'enseignant', orphanRemoval: true)]
+    private Collection $enseignes;
+
+    /**
+     * @var Collection<int, Reservation>
+     */
+    #[ORM\OneToMany(targetEntity: Reservation::class, mappedBy: 'enseignant')]
+    private Collection $reservations;
+
+    public function __construct()
+    {
+        $this->enseignes = new ArrayCollection();
+        $this->reservations = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Enseigne>
+     */
+    public function getEnseignes(): Collection
+    {
+        return $this->enseignes;
+    }
+
+    public function addEnseigne(Enseigne $enseigne): static
+    {
+        if (!$this->enseignes->contains($enseigne)) {
+            $this->enseignes->add($enseigne);
+            $enseigne->setEnseignant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEnseigne(Enseigne $enseigne): static
+    {
+        if ($this->enseignes->removeElement($enseigne)) {
+            // set the owning side to null (unless already changed)
+            if ($enseigne->getEnseignant() === $this) {
+                $enseigne->setEnseignant(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Reservation>
+     */
+    public function getReservations(): Collection
+    {
+        return $this->reservations;
+    }
+
+    public function addReservation(Reservation $reservation): static
+    {
+        if (!$this->reservations->contains($reservation)) {
+            $this->reservations->add($reservation);
+            $reservation->setEnseignant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReservation(Reservation $reservation): static
+    {
+        if ($this->reservations->removeElement($reservation)) {
+            // set the owning side to null (unless already changed)
+            if ($reservation->getEnseignant() === $this) {
+                $reservation->setEnseignant(null);
+            }
+        }
+
+        return $this;
     }
 }
--- src/Entity/Enseigne.php
+++ src/Entity/Enseigne.php
@@ -13,8 +13,40 @@ class Enseigne
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\ManyToOne(inversedBy: 'enseignes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Matiere $matiere = null;
+
+    #[ORM\ManyToOne(inversedBy: 'enseignes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Enseignant $enseignant = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getMatiere(): ?Matiere
+    {
+        return $this->matiere;
+    }
+
+    public function setMatiere(?Matiere $matiere): static
+    {
+        $this->matiere = $matiere;
+
+        return $this;
+    }
+
+    public function getEnseignant(): ?Enseignant
+    {
+        return $this->enseignant;
+    }
+
+    public function setEnseignant(?Enseignant $enseignant): static
+    {
+        $this->enseignant = $enseignant;
+
+        return $this;
     }
 }
